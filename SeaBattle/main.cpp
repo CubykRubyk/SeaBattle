cde3547a338617d82acfff36612a3ee9d946ce00
CutitaruAdrian <cutitaru.adryan@gmail.com>
2016-05-19 01:31:49 +0300

    actualizare main.cpp

--- SeaBattle/main.cpp
+++ SeaBattle/main.cpp
@@ -1,10 +1,30 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include "mainwindow.h"
+#include <QHostAddress>
+#include "chatserver.h"
 
-int main(int argc, char *argv[])
+
+
+static const quint16 DEFAULT_PORT = 6667;
+
+int main(int argc, char* argv[])
 {
+
     QApplication a(argc, argv);
-    MainWindow w;
+     MainWindow w;
+    quint16 port = 0;
+    bool ok = false;
+
+    if (argc > 0)
+    {
+        port = QString(argv[0]).toInt(&ok);
+    }
+    if (!ok)
+    {
+        port = DEFAULT_PORT;
+    }
+    ChatServer server;
+    server.listen(QHostAddress::Any, port);
     w.show();
 
     return a.exec();
