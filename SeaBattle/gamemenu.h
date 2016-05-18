4ce0b750e0cc3452bcef384bbde8c36a926ede7c
DrutaAlexandru <drutasandu@gmail.com>
2016-05-19 02:17:35 +0300

    adaugare header la gamemenu

--- SeaBattle/gamemenu.h
+++ SeaBattle/gamemenu.h
@@ -1,4 +1,34 @@
 #ifndef GAMEMENU_H
 #define GAMEMENU_H
 
+#include <QWidget>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QLabel>
+
+//clasa meniului jocului
+
+class GameMenu : public QWidget
+{
+    Q_OBJECT
+
+private:
+    QPushButton *createGame; //butonul "New Game"
+    QPushButton *connectToGame; //butonul "Connect"
+
+    QLabel      *gameLabel;
+
+public:
+    explicit GameMenu(QWidget *parent = 0);
+
+
+signals:
+    void createGameClicked(); //semnalul butonului "New Game"
+    void connectToGameClicked(); //semnalul butonului "Connect"
+
+
+public slots:
+};
+
 #endif // GAMEMENU_H
