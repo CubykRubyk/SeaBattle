3b0af6edcf6fcbcd6ad8c5f4998984c96bec4dcb
CutitaruAdrian <cutitaru.adryan@gmail.com>
2016-05-18 17:06:17 +0300

    add game menu .h + .cpp

---
+++ SeaBattle/gamemenu.cpp
@@ -0,0 +1,33 @@
+#include "gamemenu.h"
+
+GameMenu:: GameMenu (QWidget *parent):QWidget(parent)
+{
+    //aranjamentul windgetului
+
+    QVBoxLayout *layout=new QVBoxLayout(this);
+    QVBoxLayout *nameLayout= new QVBoxLayout();
+    gameLabel=new QLabel(tr("*SEA BATTLE*"));
+    QFont font;
+    font.setBold(true);
+    font.setPixelSize(25);
+    gameLabel->setFont(font);
+    createGame=new QPushButton(tr("New Game"));
+    connectToGame=new QPushButton(tr("Connect"));
+
+    nameLayout->addStretch();
+    nameLayout->addWidget(gameLabel);
+    nameLayout->addStretch();
+    layout->addLayout(nameLayout);
+
+    //adaugam butoanele
+    layout->addWidget(createGame);
+    layout->addWidget(connectToGame);
+
+    layout->addStretch();
+
+    //conectam semnalele si slot-urile
+    connect(createGame,SIGNAL(clicked()),this,SIGNAL(createGameClicked()));
+    connect(connectToGame,SIGNAL(clicked()),this,SIGNAL(connectToGameClicked()));
+
+
+}
--- SeaBattle/gamemenu.h
+++ SeaBattle/gamemenu.h
@@ -1,4 +1,33 @@
 #ifndef GAMEMENU_H
 #define GAMEMENU_H
 
+#include <QWidget>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QLabel>
+
+//clasa meniului jocului
+
+class GameMenu: public QWidget
+
+{    Q_OBJECT
+
+    private:
+       QPushButton *createGame;  //butonul "NewGame"
+       QPushButton *connectToGame; //butonul "connect"
+
+       QLabel      *gameLabel;
+
+    public:
+       explicit GameMenu(QWidget *parent=0);
+
+    signals:
+       void creatGameClicked(); //semnalul butonului "New Game"
+       void connectToGameClicked(); //semnalul butonului "Connect"
+
+    public slots:
+
+};
+
 #endif // GAMEMENU_H
